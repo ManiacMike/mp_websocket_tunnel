46c49ff2b9c5da9205397af4d3a621a5378e35f5
ManiacMike <zhangfan106975@pwrd.com>
2018-08-08 17:57:18 +0800

    fix

--- client.go
+++ client.go
@@ -35,6 +35,9 @@ var (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return false
+	},
 }
 
 // Client is a middleman between the websocket connection and the hub.
